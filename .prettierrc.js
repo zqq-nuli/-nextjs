876117bf9e250ad2703e718cd6ac306bdc11fbb5
zqq-nuli <1852150449@qq.com>
2024-05-24 00:54:13 +0800

    chore: first commit

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+ export default {
+  arrowParens: "avoid",
+  bracketSpacing: true,
+  endOfLine: "lf",
+  jsxBracketSameLine: false,
+  printWidth: 100,
+  proseWrap: "preserve",
+  semi: true,
+  tabWidth: 2,
+  useTabs: false,
+  trailingComma: "es5",
+  parser: "typescript",
+  // 结尾默认添加分号
+};
